4c8cdf8cf2437e0688ea1cd77d6dd75e2187ebf5
prasanth18prabhu <prasanth18prabhu@gmail.com>
2023-12-28 19:47:16 +0530

    commit

--- src/test/java/com/stepdefinition/Steps.java
+++ src/test/java/com/stepdefinition/Steps.java
@@ -1,6 +1,7 @@
 package com.stepdefinition;
 
 public class Steps {
-public static void main(String[] args){
-  System.out.println("Success");
+public static void main(String[] args) {
+	System.out.println("Pass");
+}
 }
